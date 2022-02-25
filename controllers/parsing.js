e1180ea5a9932e4b747aa924133c4161364c79a7
GAROUI <mathys.garoui@etu.univ-amu.fr>
2022-02-25 16:00:29 +0100

    Fonction parse dans parsing.js

--- controllers/parsing.js
+++ controllers/parsing.js
@@ -1,7 +1,11 @@
 function parse(str) {
     let obj = JSON.parse(str);
-    obj.timestamp = new Date(obj.timestamp);
-    
+    for(let capteur of obj.capteurs){
+        capteur.Timestamp = new Date(capteur.Timestamp);
+        if(!isNaN(capteur.Valeur)){
+           throw new Error("La valeur fournie par le capteur n'est pas une valeur numérique");
+        }
+    }
     return obj;
 }
 
